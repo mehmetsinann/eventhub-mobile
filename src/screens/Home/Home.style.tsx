9aea0479e0760c052ca5b7eeb81bf7cc99deaade
Mehmet Sinan Topal <73042487+mehmetsinann@users.noreply.github.com>
2024-01-31 16:54:20 +0300

    refactor(splash and icon): splash screen and app icons

--- src/screens/Home/Home.style.tsx
+++ src/screens/Home/Home.style.tsx
@@ -13,6 +13,7 @@ export const styles = StyleSheet.create({
   header: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
     paddingHorizontal: 20,
     paddingVertical: 10,
     marginBottom: 10,
